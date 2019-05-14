cab0cb931c83dfed3768c18893118870305b9f53
tu-trinh <alinatu.trinh@gmail.com>
2019-05-14 14:44:53 -0700

    Update CheeseCrushCanvas.java

--- CheeseCrush/src/CheeseCrushCanvas.java
+++ CheeseCrush/src/CheeseCrushCanvas.java
@@ -16,23 +16,28 @@ import javax.swing.*;
 public class CheeseCrushCanvas extends JComponent {
 	private final int WIDTH;
 	private final int HEIGHT;
-	private Image img;
+//	private Image img;
 	private int imgX;
 	private int imgY;
+	private Color grayBlue;
+	private Cheese[][] cheeseGrid;
 	
 	public CheeseCrushCanvas(int width, int height) {
 		WIDTH = width;
 		HEIGHT = height;
-		Image initImg = new ImageIcon("redLeicester.png").getImage();
-		img = initImg.getScaledInstance(450, 350, java.awt.Image.SCALE_SMOOTH);		
+		// img = Color object;
 		imgX = 100;
 		imgY = 100;
 		// somewhere to the right, then score to the left
 		// 2D arrays of candies
+		grayBlue = new Color(178, 201, 206);
 	}
 	
 	public void paintComponent(Graphics gr) {
 		Graphics2D g = (Graphics2D) gr;
-		g.drawImage(img, imgX, imgY, this);
+		g.setColor(grayBlue);
+		// draw the background here
+//		g.drawLine(50,50,150,150);
+//		g.drawImage(img, imgX, imgY, this);
 	}
 }
