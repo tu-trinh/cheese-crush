2b2991e5b88ee83e055b6bb72d534396b479109a
tu-trinh <alinatu.trinh@gmail.com>
2019-05-21 15:00:21 -0700

    Assign images

--- CheeseCrush/src/Red.java
+++ CheeseCrush/src/Red.java
@@ -3,20 +3,23 @@ import javax.swing.*;
 
 public class Red extends Cheese {
 	private Image img;
-//	private int imgX;
-//	private int imgY;
 	
 	public Red(String status) {
 		super(status);
 		if (status.equals("normal")) {
-			img = new ImageIcon("redLeicester.png").getImage().getScaledInstance(450, 350, java.awt.Image.SCALE_SMOOTH);
+			img = new ImageIcon("redLeicester.png").getImage();
+		} else if (status.equals("bomb")) {
+			img = new ImageIcon("redBomb.png").getImage();
+		} else if (status.equals("bottle")) {
+			img = new ImageIcon("redBottle.png").getImage();
+		} else if (status.equals("striped")) {
+			img = new ImageIcon("redStriped.png").getImage();
+		} else if (status.equals("wrapped")) {
+			img = new ImageIcon("redWrapped.png").getImage();
 		}
-//		imgX = 100;
-//		imgY = 100;
 	}
 	
-	public void paintComponent(Graphics gr) {
-		Graphics2D g = (Graphics2D) gr;
-		g.drawImage(img, imgX, imgY, this);
+	public Image getImg() {
+		return img;
 	}
 }
