2ff327667a15ab4008165092c6abf7cd6d49942e
tu-trinh <alinatu.trinh@gmail.com>
2019-05-21 15:00:41 -0700

    Update abstract class

--- CheeseCrush/src/Cheese.java
+++ CheeseCrush/src/Cheese.java
@@ -1,10 +1,14 @@
 import java.awt.Image;
 
 public abstract class Cheese {
-	private Image img;
 	private String status;
+	private Image img;
 	
 	public Cheese(String status) {
 		status = status;
+	}
+	
+	public Image getImg() {
+		return img;
 	}
 }
