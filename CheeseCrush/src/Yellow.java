9f5a2dfc33125e4ef0d1cdf35e67c2518e3bf8f7
tu-trinh <alinatu.trinh@gmail.com>
2019-05-28 03:03:20 -0700

    Home update

--- CheeseCrush/src/Yellow.java
+++ CheeseCrush/src/Yellow.java
@@ -3,9 +3,11 @@ import javax.swing.*;
 
 public class Yellow extends Cheese {
 	private Image img;
+	private String color;
 	
 	public Yellow(String status) {
 		super(status);
+		color = "yellow";
 		if (status.equals("normal")) {
 			img = new ImageIcon("yellowCamembert.png").getImage();
 		} else if (status.equals("bomb")) {
@@ -21,5 +23,9 @@ public class Yellow extends Cheese {
 	
 	public Image getImg() {
 		return img;
+	}
+	
+	public String getColor() {
+		return color;
 	}
 }
