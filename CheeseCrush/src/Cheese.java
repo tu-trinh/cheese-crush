632b404c4f37139e617d0bcc0c306e0f103e0c70
tu-trinh <alinatu.trinh@gmail.com>
2019-05-28 03:02:06 -0700

    Home update

--- CheeseCrush/src/Cheese.java
+++ CheeseCrush/src/Cheese.java
@@ -1,11 +1,16 @@
 import java.awt.Image;
 
+import javax.swing.ImageIcon;
+
 public abstract class Cheese {
 	private String status;
 	private Image img;
+	private String color;
 	
 	public Cheese(String status) {
-		status = status;
+		this.status = status;
+		img = new ImageIcon("colorBomb.png").getImage();
+		color = "none";
 	}
 	
 	public Image getImg() {
@@ -14,5 +19,20 @@ public abstract class Cheese {
 	
 	public String getStatus() {
 		return status;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public void changeStatus(String newStatus) {
+		this.status = newStatus;
+	}
+	
+	public boolean equals(Cheese other) {
+		if (this.getColor().equals(other.getColor())) {
+			return true;
+		}
+		return false;
 	}
 }
